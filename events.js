2b1f78a09edc66a3c0bd9d1cc493c002f1a79369
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-02-23 20:40:50 +1300

    Add function two

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -7,7 +7,7 @@ function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
   one()
-    
+
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -23,6 +23,11 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
+  two.addEventListener('mouseenter', makeGreen)
+  two.addEventListener('mouseleave', makeWhite)
+}
 
 // CREATE FUNCTION three HERE
 
