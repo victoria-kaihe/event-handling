4f05e7e47ffc9e494bf072f245edd7cc713a302a
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2018-02-23 21:13:48 +1300

    Define makeGreen function

--- events.js
+++ events.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', start)
 function start () {
   // The first example is done for you. This will change the background colour of the first div
   // when you mouse over it.
-  one();
+  one()
   two()
 
   // Your turn! Create a new function called `two`, then call it from here.
@@ -41,4 +41,8 @@ function makeBlue (evt) {
 
 function makeWhite (evt) {
   evt.target.style.backgroundColor = 'white'
+}
+
+function makeGreen (evt) {
+  evt.target.style.backgroundColor = 'green'
 }
